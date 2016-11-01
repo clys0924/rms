b494cb2c026b6515fa16d67995481e1648ac2b80
clys <devilss@vip.163.com>
2016-11-01 20:15:00 +0800

    create mongodb

--- rms-electron-direct-to-mongo/app/rms.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var Rms = (function () {
-    function Rms() {
-    }
-    return Rms;
-}());
-exports.Rms = Rms;
-//# sourceMappingURL=rms.js.map
--- rms-electron-direct-to-mongo/app/rms.ts
+++
@@ -1,8 +0,0 @@
-export class Rms{
-    _id: string
-    item: string;
-    unit: string;
-    mfrs: string;
-    num: number;
-    location: string
-}
